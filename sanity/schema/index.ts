8b3a525962662f990b1d40abb291fc3185e27798
gezzzi <146487115+gezzzi@users.noreply.github.com>
2025-12-21 23:42:13 +0900

    sanityの削除

--- sanity/schema/index.ts
+++
@@ -1,6 +0,0 @@
-import author from "./documents/author";
-import post from "./documents/post";
-import tag from "./documents/tag";
-import articlePortableText from "./objects/articlePortableText";
-
-export const schemaTypes = [post, author, tag, articlePortableText];
--- src/sanity/config.ts
+++
@@ -1,3 +0,0 @@
-import config from "../../sanity.config";
-
-export default config;
